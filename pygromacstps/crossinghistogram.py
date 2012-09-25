3403a2af66762a2c371bc804365756a2d5e47c85
WolfgangLechner <wolfgang.lechner@gmail.com>
2012-09-25 16:19:19 +0200

    comments

--- pygromacstps/crossinghistogram.py
+++ pygromacstps/crossinghistogram.py
@@ -7,6 +7,10 @@ Created on June 16, 2010
 import numpy as np
 
 class generichistogram(object):
+    """
+    A Crossing histogram can be based on a trajectory either forward or backward in time
+    histomin is the position of the interface and histomax can be chosen arbitrarily
+    """
     def __init__(self,histosize=2000,histomin=-4.0,histomax=4.0,forward=True):
         self.histo = np.zeros(histosize)
         self.histomin = histomin
@@ -16,13 +20,22 @@ class generichistogram(object):
         self.forward = forward
     
     def _getBox(self,value):
+        """
+        Convert the value to the histogrambox
+        """
         return (float(self.histosize)*(float(value) - float(self.histomin)))/(float(self.histomax-self.histomin))
     
     def _getValue(self,hbox):
+        """
+        Convert the histogram index to the according value
+        """
         return (float(hbox)*float(self.histomax-self.histomin) )/float(self.histosize) + float(self.histomin)
     
          
     def addRangeToHisto(self,value):
+        """
+        Add the value 1 in the range from value to histomax
+        """
         hbox = int(round(self._getBox(value)))
         if self.forward:
             if hbox > self.histosize - 1:
@@ -38,6 +51,9 @@ class generichistogram(object):
     
         
     def addPointToHisto(self,value):
+        """
+        Add a single point to the histogram
+        """
         hbox = int(round(self._getBox(value)))
         self.histo[hbox] += 1.0
         self.norm += 1.0
@@ -45,6 +61,9 @@ class generichistogram(object):
         
     
     def outputCrossingHisto(self,filename):
+        """
+        Print the histogram
+        """
         of = open(filename,"w")
         for i in range(self.histosize):
             of.write("%.18f %.18f\n" % (self._getValue(i),self.histo[i]/self.norm))
